f883669acd8c8954aed706506d24ec5d7ee46454
Eric Huss <eric@huss.org>
2022-09-06 12:01:42 -0700

    Try other sites

--- tests/eric.rs
+++ tests/eric.rs
@@ -56,3 +56,39 @@ fn basic_no_v2() -> Result<(), curl::Error> {
 
     Ok(())
 }
+
+#[test]
+fn crates_io_home() -> Result<(), curl::Error> {
+    let mut curl = Easy::new();
+
+    curl.url("https://crates.io/")?;
+    curl.write_function(|data| {
+        eprintln!("Got {} bytes", data.len());
+        Ok(data.len())
+    })?;
+    curl.useragent(&format!("cargo 1.65.0"))?;
+    curl.follow_location(true)?;
+    curl.perform()?;
+    let code = curl.response_code()?;
+    eprintln!("finished with {:?}", code);
+
+    Ok(())
+}
+
+#[test]
+fn static_crates() -> Result<(), curl::Error> {
+    let mut curl = Easy::new();
+
+    curl.url("https://static.crates.io/crates/thread_local/thread_local-1.1.3.crate")?;
+    curl.write_function(|data| {
+        eprintln!("Got {} bytes", data.len());
+        Ok(data.len())
+    })?;
+    curl.useragent(&format!("cargo 1.65.0"))?;
+    curl.follow_location(true)?;
+    curl.perform()?;
+    let code = curl.response_code()?;
+    eprintln!("finished with {:?}", code);
+
+    Ok(())
+}
