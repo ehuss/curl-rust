b098b1fd433676a6a4e46a5da13a2f1742ec0eb3
Eric Huss <eric@huss.org>
2022-09-06 11:16:03 -0700

    Test

--- examples/eric.rs
+++ examples/eric.rs
@@ -5,19 +5,23 @@
 
 extern crate curl;
 
-use curl::easy::Easy;
-use std::io::{stdout, Write};
+use curl::easy::{HttpVersion, Easy};
 
 fn main() -> Result<(), curl::Error> {
     let mut curl = Easy::new();
 
     curl.url("https://crates.io/api/v1/crates/thread_local/1.1.3/download")?;
     curl.write_function(|data| {
-        stdout().write_all(data).unwrap();
+        eprintln!("Got {} bytes", data.len());
         Ok(data.len())
     })?;
-
+    curl.useragent(&format!("cargo 1.65.0"))?;
+    curl.follow_location(true)?;
+    curl.http_version(HttpVersion::V2)?;
+    curl.pipewait(true)?;
     curl.perform()?;
+    let code = curl.response_code()?;
+    eprintln!("finished with {:?}", code);
 
     Ok(())
 }
